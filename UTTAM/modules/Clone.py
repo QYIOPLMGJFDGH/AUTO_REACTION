047195dfd3a66f0196a01e25a02158c4decdebfe
QYIOPLMGJFDGH <Uttamk1578@gmail.com>
2025-01-03 18:41:35 +0530

    Update Clone.py

--- UTTAM/modules/Clone.py
+++ UTTAM/modules/Clone.py
@@ -13,7 +13,7 @@ from config import API_HASH, API_ID, OWNER_ID
 from UTTAM import CLONE_OWNERS
 from UTTAM import UTTAM as app, save_clonebot_owner
 from UTTAM import db as mongodb
-from UTTAM import anony_boot
+from UTTAM import restart_mainbot
 
 CLONES = set()
 cloneownerdb = mongodb.cloneownerdb
