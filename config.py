09f03bf2a6525e2d1e5462fea97b25d629cba85e
QYIOPLMGJFDGH <Uttamk1578@gmail.com>
2025-01-03 16:00:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ API_ID = "6435225"
 # -------------------------------------------------------------
 API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", "7568502483:AAGUxHk-nde8djQqqiZMyn70xW8akLLr2Ww")
+BOT_TOKEN = getenv("BOT_TOKEN", "7677420218:AAEcDMuoWzAmCAZe2NFSKs3ZYiREf9O2EwA")
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority")
 OWNER_ID = int(getenv("OWNER_ID", "7400383704"))
 SUPPORT_GRP = "PURVI_UPDATES"
