4e5a1474fe43683d32466f309605b033a2d28db3
QYIOPLMGJFDGH <Uttamk1578@gmail.com>
2025-01-03 15:32:50 +0530

    Update Start.py

--- UTTAM/modules/Start.py
+++ UTTAM/modules/Start.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
-from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 from pyrogram.types import Message
 from UTTAM import UTTAM
